e3e1596678523fd5f89217cd9a3be80f3623b84c
Pavankumar Shivashetty <pavankumar_shivashetty@epam.com>
2022-04-05 16:17:23 +0530

    did small change

--- IslandsProblem/src/main/java/com/epam/Islands.java
+++ IslandsProblem/src/main/java/com/epam/Islands.java
@@ -1,15 +1,32 @@
 package com.epam;
 
+import java.util.Scanner;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 class Islands {
 
 	public static void main(String[] args) {
-		int[][] array = { { 1, 0, 0, 0 }, { 0, 1, 1, 0 }, { 0, 1, 0, 0 } };
-		int r = array.length;
-		int c = array[0].length;
+//		int[][] array = { { 1, 0, 0, 0 }, { 0, 1, 1, 0 }, { 0, 1, 0, 0 } };
+		int[][] array = new int[100][100];
+		
+		Scanner scan = new Scanner(System.in);
+		
+		log.info("Enter number of rows");
+		int r = scan.nextInt();
+		log.info("Enter number of columns");
+		int c = scan.nextInt();
 		int islands = 0;
+		log.info("enter elements");
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				array[i][j]=scan.nextInt();
+			}
+		}
+		
+		
+		
 		for (int i = 0; i < r; i++) {
 			for (int j = 0; j < c; j++) {
 				if (array[i][j] == 1) {
@@ -17,8 +34,11 @@ class Islands {
 					islands++;
 				}
 			}
+			
 		}
-		log.info("" + islands);
+		
+		log.info(String.valueOf(islands));
+		
 	}
 	
 	private static void traverse(int[][] array, int i, int j) {
